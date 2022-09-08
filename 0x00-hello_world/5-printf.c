7ddd5cb83dd4b70880d14b4014f6607310d4a5c8
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-08 16:05:47 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 /**
- * * I don't know why they want main here
- * * This return no be my mind sha
- * */
+ * I don't know why they want main here
+ *
+ * This return no be my mind sha
+ * 
+ * main - wetin main do?
+ * 
+ * return: the change you dey owe me
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
