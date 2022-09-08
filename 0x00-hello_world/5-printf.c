f5437de1b7502bb29dd5f7792b470f006a063c08
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-08 16:02:40 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include	<stdio.h>
+#include <stdio.h>
+
 /**
- * main - mgbao
- * Return: ifiok
- * 
- */
-int	main(void)
+ * * I don't know why they want main here
+ * * This return no be my mind sha
+ * */
+int main(void)
 {
-	printf("\with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
