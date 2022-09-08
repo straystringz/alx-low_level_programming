2a1e073c79e8a3ccf3053b6ccb5f7b65f7165fc4
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-08 15:33:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-/* This is my first C programe*/
-#include <stdio.h>
-
-int main()
+#include	<stdio.h>
+/*
+ *I am trying my best here
+ */
+int	main()
 {
 	puts("\"Programming is like building a multilingual puzzle\"\n");
 	return 0;
