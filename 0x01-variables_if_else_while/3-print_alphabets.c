4504e6e6db0480009b3e74ac9dbd54e5ede96869
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 02:19:22 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function
+ *
+ *
+ *
+ * Return: should return (0)
+ *
+ */
+
+int main(void)
+
+{
+
+	char	c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+putchar('\n');
+
+
+						return (0);
+
+}
+
+
