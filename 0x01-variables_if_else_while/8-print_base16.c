b30937aa52f40d6a0760ae24bd2ca83302710df3
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 04:35:01 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,51 @@
+#include	<stdio.h>
+
+
+
+/**
+ *
+ * main - the main function
+ *
+ *
+ *
+ * Return: if correct should return (0)
+ *
+ */
+
+int main(void)
+
+{
+
+	char	c;
+
+
+
+	for	(c = '0'; c <= '9'; c++)
+
+	{
+
+		putchar(c);
+
+	}
+
+
+
+	for	(c = 'a'; c <= 'f'; c++)
+
+	{
+
+		putchar(c);
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
+
+
