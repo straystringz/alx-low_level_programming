4090ac05ad45f64d10aa9768a43ca81b82484077
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 04:02:36 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include	<stdio.h>
+
+
+
+/**
+ *
+ * main - the function
+ *
+ * Return: if correct should return(0)
+ *
+ */
+
+int	main(void)
+
+{
+
+	int	n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
