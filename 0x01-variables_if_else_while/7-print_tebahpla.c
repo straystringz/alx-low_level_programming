40b62ddced0a566298e2b0d7bf3f0ce872bfd0f6
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 04:27:55 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - contains the main function
+ *
+ * Return: returns (0) if correct
+ *
+ */
+int	main(void)
+{
+	char	c;
+
+	for	(c = 'z'; c >= 'a'; c--)
+
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return	(0);
+}
