748c09130933f88aa5ae830bb6cd230450372480
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 03:51:27 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,37 @@
+#include	<stdio.h>
+
+
+
+/**
+ *
+ * main - this is the function
+ *
+ *
+ *
+ * Return: returns (0) if correct
+ *
+ * */
+
+int main(void)
+
+{
+
+	char	n;
+
+
+
+	for	(n = '0'; n <= '9'; n++)
+
+	{
+
+		putchar(n);
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
