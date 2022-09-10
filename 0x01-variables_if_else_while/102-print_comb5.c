2f8211ff0528526313789459687c1150c8a780d7
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-10 06:39:44 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,31 +13,31 @@ int main(void)
 
 {
 
-	int i, p;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 
 	{
 
-		for (p = 0; p < 100; i++)
+		for (j = 0; j < 100; j++)
 
 		{
 
-			if (p > i)
+			if (i < j)
 
 			{
 
-				putchar((i / 10) + '0');
+				putchar((i / 10) + 48);
 
-				putchar((i % 10) + '0');
+				putchar((i % 10) + 48);
 
 				putchar(' ');
 
-				putchar((p / 10) + '0');
+				putchar((j / 10) + 48);
 
-				putchar((p % 10) + '0');
+				putchar((j % 10) + 48);
 
-				if (i != 98)
+				if (i != 98 || j != 99)
 
 				{
 
