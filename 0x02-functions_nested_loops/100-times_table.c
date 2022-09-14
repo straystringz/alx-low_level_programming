45592505d466aa605473d2c8fd18f95460c11cbb
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-14 14:10:10 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,72 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * times table - times table function
- * @n: integer to return
- *
- */
+
+*print_times_table - prints the  times table
+
+*parameter
+
+*@n: parameter
+
+*Return: will return no value if correct
+
+*/
+
 void print_times_table(int n)
+
 {
-	int i;
-	int j;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				int prod = j * i;
+int digit, mult, result;
 
-				if (j == 0)
-				{
-					_putchar('0');
-				} else if (prod <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + prod);
-				} else if (prod > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (prod / 100));
-					_putchar('0' + ((prod / 10) % 10));
-					_putchar('0' + (prod % 10));
-				} else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (prod / 10));
-					_putchar('0' + (prod % 10));
-				}
-			}
-			_putchar('\n');
-		}
-	}
+if (n <= 15 && n >= 0)
+
+{
+
+for (digit = 0; digit <= n; digit++)
+
+{
+
+_putchar('0');
+
+
+
+for (mult = 1; mult <= n; mult++)
+
+{
+
+_putchar(',');
+
+_putchar(' ');
+
+result = digit * mult;
+
+if (result <= 99)
+
+_putchar(' ');
+
+
+
+if (result <= 9)
+
+_putchar(' ');
+
+if (result >= 100)
+
+{
+
+_putchar((result / 100) + '0');
+
+_putchar((result / 10) % 10 + '0');
+
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+
+}
+}
+}
