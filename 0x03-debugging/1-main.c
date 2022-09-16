9448eb73d7a7040f631bbfe64076d6ae713b3b53
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 05:57:35 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,16 +2,18 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
- *
  */
 
 int main(void)
 {
-int i;
+
+	int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
