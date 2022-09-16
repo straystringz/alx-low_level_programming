4b5aa7582f2fff5c5b5a13e998a0a12f48ce8c38
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 04:32:44 -0700

    Multiple Mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN.H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
-void positive_or_negative(inti);
-void print_remaining_days(int a, int b, int c,);
-int largest_number(int a, int b, int c,);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-#edif /*MAIN_H*/
+
+#endif /* MAIN_H */
