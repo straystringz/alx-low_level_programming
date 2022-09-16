4c73c21c76a429540ff850441d676bd68c800c77
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 04:36:20 -0700

    Multiple Mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,9 @@
 
 /**
  * main - prints positive or negative integer
+ *
  * Return: 0
+ *
  */
 
 int main(void)
@@ -10,6 +12,7 @@ int main(void)
 {
 	int i;
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
