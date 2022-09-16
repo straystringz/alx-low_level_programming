5805408229259d96df2d6d297f25b10825daad45
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 04:09:27 -0700

    Multiple Mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
-* main - prints integer or a negative integer
-* 
-* Return: always 0
-*
-*/
+ * main - prints integer or a negative integer
+ *
+ * Return: 0
+ *
+ * */
 
 int main(void)
 
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
 }
