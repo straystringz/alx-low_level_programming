99ed5dc9907302d5c41918572f359c1ff376ca76
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 05:55:15 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,69 +1,17 @@
-#include "main.h"
-
-
+#include <stdio.h>
 
 /**
-
- * largest_number - returns the largest of 3 numbers
-
- * @a: first integer
-
- * @b: second integer
-
- * @c: third integer
-
- * Return: largest number
-
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ *
  */
 
-
-
-int largest_number(int a, int b, int c)
-
+int main(void)
 {
-
-int largest;
-
-
-
-if (a > b && a > c)
-
-{
-
-largest = a;
-
-}
-
-else
-
-if (b > a && b > c)
-
-{
-
-largest = b;
-
-}
-
-else
-
-if (c > b)
-
-{
-
-largest = c;
-
-}
-
-else
-
-{
-
-largest = b;
-
-}
-
-
-
-return (largest);
-
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
