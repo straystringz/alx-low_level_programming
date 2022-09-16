ab897136638faa99c054620a9e5fbf2df6b6da4e
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-16 03:55:37 -0700

    Multiple Mains

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+__Readme about Debugging__
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN.H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(inti);
+void print_remaining_days(int a, int b, int c,);
+int largest_number(int a, int b, int c,);
+int convert_day(int month, int day);
+#edif /*MAIN_H*/
