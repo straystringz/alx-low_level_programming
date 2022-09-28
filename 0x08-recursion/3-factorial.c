6c6d6a461cbc54ec30a1a9edbef88f29e4a729c6
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-28 16:57:11 -0700

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *factorial - factorial of a given number
+ *@n: memory address to fill
+ *Return: factorial
+ */
+
+int factorial(int n)
+
+{
+
+if (n == 0) /
+
+return (1);
+
+else if (n < 0) /
+return (-1);
+else
+return (n * factorial(n - 1));
+
+}
