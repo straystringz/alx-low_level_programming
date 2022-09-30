7b1c704e7c395fe2c74d27a9bde090889757d374
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-09-30 16:18:20 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strlen - returns the length of a string
- *
- *   * @s: string
- *
- *    * Return: length of the string
- *
- *     */
-
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
 int _strlen(char *s)
-
 {
+	int len = 0;
 
-		int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
-
-
-			while (*s != '\0')
-
-					{
-
-								len++;
-
-										s++;
-
-											}
-
-
-
-				return (len);
-
+	return (len);
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,10 @@
-lines (12 sloc)  247 Bytes
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - waaaa
+ * @c: Santos
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
 int _putchar(char c)
 {
