4def4a7e895eb3b7fbe2b7383562f79ddd44b137
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-10-10 14:54:34 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-/**
-* File: 3-function_like_macro.h
-* @x:parameter for macro
-* Desc:Header file that describes function_like_macto
-*/
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
+#ifndef ABS_H
+#define ABS_H
+#define ABS(a) (((a) < 0) ? (-(a)) : (a))
 #endif
