7dd8c8ed9d3c651957ecb72d289e227573def54d
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-10-10 14:47:27 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,11 @@
-#ifndef SUM_DEF
-#define SUM_DEF
-#define SUM(x, y) (x + y)
-#endif /*end SUM_DEF*/
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+* File: 3-function_like_macro.h
+* @x:parameter for macro
+* Desc:Header file that describes function_like_macto
+*/
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
