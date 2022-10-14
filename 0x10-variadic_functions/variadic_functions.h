9d9a284a43a3837daaf108fb3900da9737ab3abb
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2022-10-13 19:38:55 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,24 @@
-#ifndef VARFUNC_H
-#define VARFUNC_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+/*
+* File: variadic_functions.h
+* Desc: Header file containing prototypes for all functions
+*/
 #include <stdarg.h>
+/**
+* struct printer - A new struct type defining a printer.
+* @symbol: A symbol representing a data type.
+* @print: a pointer function to a function
+*/
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct type - Struct data type
- *
- * @op: data type argument
- * @f: The function associated
- */
-
-typedef struct type
-{
-	char *op;
-	void (*f)(va_list, char *);
-} type_t;
 
 #endif
