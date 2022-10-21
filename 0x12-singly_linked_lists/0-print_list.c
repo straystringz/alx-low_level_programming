c688f295497a84c1d867cb78b0232275e9f5ebd0
Victor Emmanuel <62056934+straystringz@users.noreply.github.com>
2022-10-21 03:35:17 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,14 +3,14 @@
 #include "lists.h"
 
 /**
-  * print_list - Prints all elements of a list
-  * @h: A linked list
-  * Return: The number of nodes
-  */
+* print_list - Prints all elements of a list
+* @h: A linked list
+* Return: The number of nodes
+*/
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
