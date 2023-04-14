1bb7759faf3cd6786b82a0a332ff0d5abebc9edc
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-14 01:38:38 -0700

    search_algos.h update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,94 +7,41 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer value stored in the node
- * @next: pointer to the next node in the list
+ * @n: integer stored in node
+ * @next: pointer to next node in list
  *
- * Description: node structure for a singly linked list
+ * Description: singly linked using list node structure
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 /**
- * struct skiplist_s - skip list
- * @n: integer value stored in the node
- * @express: pointer to the next node in the express lane
- * @next: pointer to the next node
+ * struct skiplist_s - skips list
+ * @n: integer stored in node
+ * @express: points to the next node in the express lane
+ * @next: points to the next node
  *
- * Description: node structure for a singly linked list with an express lane
+ * Description: singly linked list using  express lane node structure
  */
 typedef struct skiplist_s
 {
-	int n;
-	struct skiplist_s *express;
-	struct skiplist_s *next;
+    int n;
+    struct skiplist_s *express;
+    struct skiplist_s *next;
 } skiplist_t;
 
 /* Functions */
 
-/**
- * linear_search - searches for a value in an array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int linear_search(int *array, size_t size, int value);
-
-/**
- * binary_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int binary_search(int *array, size_t size, int value);
-
-/**
- * jump_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int jump_search(int *array, size_t size, int value);
-
-/**
- * interpolation_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int interpolation_search(int *array, size_t size, int value);
-
-/**
- * exponential_search - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int exponential_search(int *array, size_t size, int value);
-
-/**
- * advanced_binary - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: index of the value in the array, or -1 if not found
- */
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-/**
- * jump_list - searches for a value in a singly linked list of ints
- * @list:
+#endif /* SORTING_ALGOS_H */
