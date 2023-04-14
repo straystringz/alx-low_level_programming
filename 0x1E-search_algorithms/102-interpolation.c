0526b5403a5a5e5d8fc303feea1cfb907d41dbe3
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-14 04:33:29 -0700

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,42 +12,25 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low, high;
-	double f;
+    size_t low = 0, high = size - 1, pos;
 
-	if (array == NULL)
-		return (-1);
+    if (array == NULL || size == 0)
+        return (-1);
 
-	low = 0;
-	high = size - 1;
+    while (low <= high && value >= array[low] && value <= array[high])
+    {
+        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-	while (size)
-	{
-		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
-		printf("Value checked array[%d]", (int)pos);
+        if (array[pos] == value)
+            return (pos);
 
-		if (pos >= size)
-		{
-			printf(" is out of range\n");
-			break;
-		}
-		else
-		{
-			printf(" = [%d]\n", array[pos]);
-		}
+        if (array[pos] < value)
+            low = pos + 1;
+        else
+            high = pos - 1;
+    }
 
-		if (array[pos] == value)
-			return ((int)pos);
-
-		if (array[pos] < value)
-			low = pos + 1;
-		else
-			high = pos - 1;
-
-		if (low == high)
-			break;
-	}
-
-	return (-1);
+    return (-1);
 }
+
