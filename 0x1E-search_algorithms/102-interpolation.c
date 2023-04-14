29c6e52678b04f2343b05807ddd54af946de4925
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-14 04:45:08 -0700

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,25 +12,24 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low = 0, high = size - 1, pos;
+	size_t low = 0, high = size - 1, pos;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    while (low <= high && value >= array[low] && value <= array[high])
-    {
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value)
-            return (pos);
+		if (array[pos] == value)
+			return (pos);
 
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
-    }
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
-
