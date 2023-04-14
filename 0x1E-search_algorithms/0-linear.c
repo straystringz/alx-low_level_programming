aa1f7f4a46e6f0a203f744960f4391497513f036
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-14 01:51:15 -0700

    Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,21 +16,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    if (array == NULL) {
+        return -1;
+    }
 
-	if (array == NULL)
-		return (-1);
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
-		if (array[i] == value)
-		{
-			printf("Found %d at index: %lu\n", value, i);
-			return (i);
-		}
-	}
-	printf("Value %d not found\n", value);
-	return (-1);
+    return -1;
 }
