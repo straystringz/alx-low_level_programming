f2496ff37ce94d32fe77e5234c23464ef6d373ea
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-14 02:23:22 -0700

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,25 +15,24 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t i, mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t i, mid;
 
-    while (left <= right)
-    {
-        printf("Searching in array:");
-        for (i = left; i < right; i++)
-            printf(" %d,", array[i]);
-        printf(" %d\n", array[right]);
+	while (left <= right)
+	{
+		printf("Searching in array:");
+		for (i = left; i < right; i++)
+			printf(" %d,", array[i]);
+		printf(" %d\n", array[right]);
 
-        mid = (left + right) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return (-1);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
-
